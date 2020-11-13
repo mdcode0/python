4eae486a68a2f3669a9fac6c9e1523248a13fde6
muhammad096 <74395421+muhammad096@users.noreply.github.com>
2020-11-13 11:39:45 +0530

    Create 02_HelloWorld.py

---
+++ 02_HelloWorld.py
@@ -0,0 +1,12 @@
+# Printing any number
+
+print(490)
+
+# Printing any string
+
+print("Hi")
+
+############################################### End ########################################
+
+# 1. Print any number value ?
+# 2. Print any string value ?
